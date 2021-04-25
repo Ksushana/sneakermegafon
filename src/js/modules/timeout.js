2873a53a2e3505c98449de53bd527d690f811c90
Ksenia Balashova <utyatka@gmail.com>
2021-04-25 18:48:11 +0200

    timer

--- src/js/modules/timeout.js
+++ src/js/modules/timeout.js
@@ -1,11 +1,17 @@
 const timer = document.querySelector(".timeout__timer");
+const title = document.querySelector(".timeout__title");
 const countDownDate = new Date(timer.dataset.time).getTime();
+const countDownDateAfter = new Date(timer.dataset.timeClose).getTime();
 
 const x = setInterval(function() {
 
   const now = new Date().getTime();
 
-  const distance = countDownDate - now;
+  const distanceBefore = countDownDate - now;
+  const distanceAfter = countDownDateAfter - now;
+
+
+  const distance = distanceBefore > 0 ? distanceBefore : distanceAfter;
 
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -14,10 +20,10 @@ const x = setInterval(function() {
   document.getElementById("hours").innerHTML = formatTime(hours);
   document.getElementById("min").innerHTML = formatTime(minutes);
   document.getElementById("sec").innerHTML = formatTime(seconds);
+  title.innerHTML = ("РЕГИСТРАЦИЯ ОТКРОЕТСЯ ЧЕРЕЗ");
 
-  if (distance < 0) {
-    clearInterval(x);
-    // document.getElementById("demo").innerHTML = "EXPIRED";
+  if (distanceBefore < 0) {
+    title.innerHTML = ("РЕГИСТРАЦИЯ ЗАКРОЕТСЯ ЧЕРЕЗ");
   }
 }, 1000);
 
